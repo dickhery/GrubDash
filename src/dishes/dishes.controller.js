9d076e874471418b227b611fd7a03e71dc588875
Richard Hery <dickhery@gmail.com>
2024-05-20 06:02:45 -0700

    Implemented POST /dishes handler with create method and added route to dishes.router

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,11 +7,21 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+const validateDish = require("../middleware/validateDish");
+const dishExists = require("../middleware/dishExists");
 
 const list = (req, res) => {
     res.json({ data: dishes });
 };
 
+const create = (req, res) => {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    const newDish = { id: nextId(), name, description, price, image_url };
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+};
+
 module.exports = {
     list,
+    create: [validateDish, create],
 };
